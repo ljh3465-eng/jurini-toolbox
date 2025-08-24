7239a8cb35592599a28c3e514976f74ee8c3689b
ljh3465 <ms5313@naver.com>
2025-08-24 17:45:25 +0900

    네이버서치어드바이져 추가2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,10 @@ export const metadata: Metadata = {
   title: "주린이 필수 툴박스",
   description: "주린이를 위한 물타기 계산기, 배당금 계산기, 투자 용어 사전 등 유용한 주식 투자 도구 모음.",
   verification: {
-    naver: "fcfee74366a02e63eea409edcc3dd7cce915673b",
-  },
+    other: {
+      "naver-site-verification": "fcfee74366a02e63eea409edcc3dd7cce915673b",
+    },
+  };
 };
 
 export default function RootLayout({
